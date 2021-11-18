847f1bb80d5ef673b529b6c55e2a3fa8081c0772
Eitautas <bc@wfh.lt>
2021-11-18 20:09:44 +0200

    Update updateAallContainers.sh

--- updateAallContainers.sh
+++ updateAallContainers.sh
@@ -8,7 +8,8 @@ function update_container() {
   container=$1
   echo "Updating $container..."
   # to chain commands within one exec we will need to wrap them in bash
-  pct exec $container -- bash -c "apt update && apt upgrade -y"
+  pct exec $container -- bash -c "apt update && apt upgrade -y && apt autoremove -y"
+  pct exec $container -- bash -c "apk update && apk upgrade -y"
 }
 
 for container in $containers
